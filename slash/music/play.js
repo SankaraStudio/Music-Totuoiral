18748fa9ce386542b36cf3c1cf4766c97057c513
SankaraStudio <90615106+SankaraStudio@users.noreply.github.com>
2021-09-28 22:42:55 +0700

    Add files via upload

--- slash/music/play.js
+++ slash/music/play.js
@@ -1,1 +1,39 @@
+let { CommandInteraction, Client } = require("discord.js");
 
+module.exports = {
+  name: "play",
+  description: "play music",
+  type: "CHAT_INPUT",
+  options: [
+    {
+      name: "song",
+      description: "the song you want to play",
+      type: 3,
+      required: true
+    }
+  ],
+  permission: {
+    bot: [],
+    channel: []
+  },
+  /*
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   */
+  start: async function(client, interaction) {
+    let options = interaction.options._hoistedOptions;
+
+    let channel = interaction.member.voice.channel;
+    let song = options.find(x => x.name === "song").value;
+
+    client.music.play({
+      interaction: interaction,
+      channel: channel,
+      song: song
+    });
+
+	  await interaction.reply({
+      content: `Searching ${song}...`
+    });
+	}
+};
